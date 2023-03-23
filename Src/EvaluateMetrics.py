16c438365672f9252c4743152cd14e51efe30305
bi11-152vyl <linhvy.bi11-152@st.usth.edu.vn>
2023-03-23 08:54:06 +0700

    add example for evaluation compute

--- Src/EvaluateMetrics.py
+++ Src/EvaluateMetrics.py
@@ -1,19 +1,28 @@
 import Levenshtein
 
-# đoạn văn bản gốc
+# Original
 #reference = "Full name: BUI HUY HOANG\nDoB: 11/12/2003\tGender: Male\nIntake: 2021-2024\tCourse: Bachelor\nMajor: Information and Communication\nTechnology\n\nBI12-170\nValidity: 30/10/2024"
 
-# đoạn văn bản
-reference = "Họ và tên: LẠI HẢI ANH\nNgày sinh: 15/01/2002 Giới tính: Nữ\nKhóa học: 2020-2024 Hệ:Cử nhân\nNgành học: An toàn thông tin\n\nBA11-004 \nHSD: 31/10/2024"
+#reference = "Họ và tên: LẠI HẢI ANH\nNgày sinh: 15/01/2002 Giới tính: Nữ\nKhóa học: 2020-2024 Hệ:Cử nhân\nNgành học: An toàn thông tin\n\nBA11-004 \nHSD: 31/10/2024"
 
-# đoạn văn bản được nhận diện bởi Tesseract OCR
-hypothesis = "Họ và tên: LẠI HẢI ANH\n\nNgày sinh: 15/01/2002 Giới tinh: Nar\nKhóa học: 2020-2024 Hệ:Cử nhân\nNgành học: An toàn thông tin\n\nBA11-004 4\nHSD: 31/10/2024 :r ]"
+#reference = "Full name: PHAM DUC ANH\nDoB: 30/09/2002 Gender: Male\nIntake: 2020-2023 Course: Bachelor\nMajor: Information Technology and Communication\n\nBI11-021\nValidity: 31/10/2023"
+#reference = "Full name: NGUYEN DUC MINH\nDoB: 14/03/2002 Gender: Male\nIntake: 2020-2023 Course: Bachelor\nMajor: Information Technology and Communication\n\nBI11-163\nValidity: 31/10/2023"
+#reference = "Họ và tên: NGUYỄN ĐỨC MINH\nNgày sinh: 14/03/2002 Giới tính: Nam\nKhóa học: 2020-2023 Hệ:Cử nhân\nNgành học: Công nghệ thông tin và Truyền thông"
+reference ="Họ và tên:TRẦN NGỌC BÁCH\nNgày sinh:11/12/2001  Giới tính:Nam\nHệ đào tạo:CỬ NHÂN    Khóa: 2019-2023\nNgành:CÔNG NGHỆ THÔNG TIN VÀ TRUYỀN THÔNG\n\nMSV:BA10-004"
 
 
-# tính WER
+# Genereated by Tesseract OCR
+#hypothesis = "Họ và tên: LẠI HẢI ANH\n\nNgày sinh: 15/01/2002 Giới tinh: Nar\nKhóa học: 2020-2024 Hệ:Cử nhân\nNgành học: An toàn thông tin\n\nBA11-004 4\nHSD: 31/10/2024 :r ]"
+#hypothesis = "Full name:PHAM DUC ANH\n\nDoB: 30/09/2002 Gender: Male\nIntake: 2020-2023 Course: Bachelor\nMajor: Information Technology and Communication\n\nBI11-021\n\nValidity: 31/10/2023\n7 d"
+#hypothesis = "Full name:NGUYEN DUC MINH\n\nDoB: 14/03/2002 Gender: Male\nIntake: 2020-2023 Course: Bachelor\nSN Major: —_[nformation Technology and Communication\n\nBI11-163\n\nValidity: 31/10/2023"
+#hypothesis = "BI1-163\nHSD: 31/10/2023\n\nTHẺ SINH VIÊN\n\nHọ và tên: NGUYÊN ĐỨC MINH\n\nNgày sinh: 14/03/2002 Giới tính:Nam\nKhóahọc: 2020-2023. Hệ:Cử nhân\nNgành học: Công nghệ thông tin và Truyền thông"
+hypothesis = "Họ và tên: TRAN NGOC BACH\nNgày sinh: 11/12/2001 Giới tính: Nam\nHệ đào tạo: CỦ NHÂN Khóa: 2019-2023\n\nNgành: CÔNG NGHỆ THÔNG TIN VÀ TRUYỀN THÔNG\n\nxsv.sasss - ||||INIIWIIM"
+
+
+# calculate WER
 wer = Levenshtein.distance(reference.split(), hypothesis.split()) / len(reference.split())
 
-# tính CER
+# calculate CER
 cer = Levenshtein.distance(reference.replace(' ', '').replace('\n', ''), hypothesis.replace(' ', '').replace('\n', '')) / len(reference.replace(' ', '').replace('\n', ''))
 
 print("WER:", wer)
--- Src/WER_CER evaluation.py
+++ Src/WER_CER evaluation.py
@@ -1,21 +1,21 @@
-# nhập danh sách giá trị WER và CER
-wer_list = list(map(float, input("Nhập danh sách giá trị WER, các giá trị cách nhau bởi khoảng trắng: ").split()))
-cer_list = list(map(float, input("Nhập danh sách giá trị CER, các giá trị cách nhau bởi khoảng trắng: ").split()))
+# Enter the list of WER and CER values
+wer_list = [0.19047619047619047, 0.19047619047619047, 0.3333333333333333, 0.19230769230769232]
+cer_list = [0.033783783783783786, 0.033783783783783786, 0.033783783783783786, 0.059322033898305086]
 
-# tính giá trị trung bình của WER và CER
+# Calculate the mean values of WER and CER
 wer_mean = sum(wer_list) / len(wer_list)
 cer_mean = sum(cer_list) / len(cer_list)
 
-# in ra giá trị trung bình của WER và CER
-print(f"Giá trị trung bình của WER: {wer_mean}")
-print(f"Giá trị trung bình của CER: {cer_mean}")
+# Print out the mean values of WER and CER
+print(f"Mean value of WER: {wer_mean}")
+print(f"Mean value of CER: {cer_mean}")
 
-# so sánh giá trị trung bình với tiêu chuẩn và quy chuẩn chung
+# Compare the mean values with the standard criteria
 if wer_mean < 0.1 and cer_mean < 0.1:
-    print("Model OCR rất tốt")
+    print("The OCR model is very good")
 elif wer_mean < 0.3 and cer_mean < 0.3:
-    print("Model OCR tốt")
+    print("The OCR model is good")
 elif wer_mean < 0.5 and cer_mean < 0.5:
-    print("Model OCR khá tốt")
+    print("The OCR model is quite good")
 else:
-    print("Model OCR còn phải cải thiện") 
+    print("The OCR model needs improvement")
